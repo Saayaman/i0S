5c82c959cad142cd720412505c6ec081f94cba2b
Saayaman <ayakosayama1987@gmail.com>
2017-06-06 14:29:12 -0700

    Draw app

---
+++ SegmentedControl/SegmentedControl/ViewController.swift
@@ -0,0 +1,33 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var segControl: UISegmentedControl!
+    
+    @IBOutlet weak var orangeBG: UIView!
+    @IBOutlet weak var blueBG: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        
+            switch sender.selectedSegmentIndex {
+            case 0:
+                orangeBG.isHidden = true
+                blueBG.isHidden = false
+            case 1:
+                orangeBG.isHidden = false
+                blueBG.isHidden = true
+            default:
+                break;
+            }
+    }
+
+}
+
