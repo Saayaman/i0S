fac5f8d0f126ae8130be8123eae213fd28a10c09
Saayaman <ayakosayama1987@gmail.com>
2017-06-14 22:18:53 -0700

    Added UIPageControl

--- ImageGallery/ImageGallery/ViewController.swift
+++ ImageGallery/ImageGallery/ViewController.swift
@@ -4,71 +4,84 @@
 //
 //  Created by ayako_sayama on 2017-06-13.
 //  Copyright © 2017 ayako_sayama. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController, UIScrollViewDelegate, UIGestureRecognizerDelegate{
+class ViewController: UIViewController, UIScrollViewDelegate, UIGestureRecognizerDelegate, UIPageViewControllerDelegate{
 
-    let cellidentifier = "cell"
-    let images = ["sea1","sea2","sea3","sea4","sea5","sea6","sea7"]
-    var imageViews:[UIImageView] = []
-    var imageView1: UIImageView!
-    
-    
     @IBOutlet weak var scrollView: UIScrollView!
-    
     @IBOutlet weak var pageControll: UIPageControl!
+    
+    
+    var scrollWidth : CGFloat = UIScreen.main.bounds.size.width
+    var scrollHeight : CGFloat = UIScreen.main.bounds.size.height
+    
+    let cellidentifier = "cell"
+    let images = ["sea1","sea2","sea3","sea4","sea5"]
+    var imageViews:[UIImageView] = []
+//    var imageView1: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         scrollView.delegate = self
-        scrollView.contentSize = CGSize(width: view.bounds.width * 3, height: view.bounds.height)
         
-        
+        scrollView.contentSize = CGSize(width: (scrollWidth * 5), height: scrollHeight)
+        scrollView.isPagingEnabled = true
         
         for (index,image) in images.enumerated(){
             let imageV = setImage(name: image, tag: index)
             imageViews.append(imageV)
             scrollView.addSubview(imageV)
         }
+        
+        view.insertSubview(pageControll, at: 0)
+        view.bringSubview(toFront: pageControll)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-    }
-
     func setImage(name:String, tag: Int)-> UIImageView{
+        
+
         let imageView = UIImageView(image: UIImage(named: name))
         imageView.tag = tag
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor.red
+        
+        imageView.frame = CGRect(x: scrollWidth * CGFloat(imageView.tag + 1), y: 0, width: scrollWidth, height: scrollHeight)
+        
+        print("xpotision tag0: \(scrollWidth * CGFloat(tag))")
+        imageView.backgroundColor = UIColor.red
+        
+        
         imageView.isUserInteractionEnabled = true
-        scrollView.addSubview(imageView)
         
-        
-        if imageView.tag == 0{
-            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20).isActive = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
 
-        } else {
-            let previousView = imageViews[imageView.tag - 1]
-            imageView.leadingAnchor.constraint(equalTo: previousView.trailingAnchor, constant: 20).isActive = true
-        }
+//        
+//        if imageView.tag == 0{
+//            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0).isActive = true
+//
+//        } else {
+//            let previousView = imageViews[imageView.tag - 1]
+//            imageView.leadingAnchor.constraint(equalTo: previousView.trailingAnchor, constant: 0).isActive = true
+//        }
         
-        imageView.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor, constant: 20).isActive = true
-        imageView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.4).isActive = true
-        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1).isActive = true
-        
-        
+//        imageView.widthAnchor.constraint(equalToConstant: scrollWidth).isActive = true
+//        imageView.heightAnchor.constraint(equalToConstant: scrollHeight).isActive = true
+//
+
         let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.tap(_:)))
         tap.delegate = self
-        
         imageView.addGestureRecognizer(tap)
-        
+        scrollView.addSubview(imageView)
+
         return imageView
     }
     
@@ -78,13 +91,28 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIGestureRecognize
         performSegue(withIdentifier: "DetailsViewController", sender: sender.view?.tag)
     }
     
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as! DetailsViewController
         
         if let tag = sender as? Int {
             vc.senderTag = tag
         }
+    }
+    
+    
+    @IBAction func changePage(_ sender: UIPageControl) {
+        scrollView.scrollRectToVisible(CGRect(x: scrollWidth * CGFloat(pageControll.currentPage), y: 0, width: scrollWidth, height: scrollHeight), animated: true)
+        
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        setIndiactorForCurrentPage()
+    }
+    
+    func setIndiactorForCurrentPage()  {
+        let page = (scrollView?.contentOffset.x)!/scrollWidth
+        pageControll?.currentPage = Int(page)
+        
     }
 
 }
