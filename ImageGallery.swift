78d552eaf92147b37e34279063c0313ee726980c
Saayaman <ayakosayama1987@gmail.com>
2017-07-17 16:04:33 -0700

    Change

--- ImageGallery.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ImageGallery.swift
-//  
-//
-//  Created by ayako_sayama on 2017-06-13.
-//
-//
-
-import Foundation
--- ImageGalleryPractice/ImageGalleryPractice/Bird.swift
+++
@@ -1,9 +0,0 @@
-class Bird {
-    var name:String?
-    var group:String?
-    
-    init(name: String, group: String) {
-        self.name = name
-        self.group = group
-    }
-}
