48f86bc1dc3a7b95a6ecfd999775a11c9e08b9f1
AYAKO SAYAMA <ayakosayama@AYAKOs-MacBook-Pro.local>
2017-09-04 18:50:47 -0700

    start firebaseAuth over

--- FirebaseAuth/Pods/Target Support Files/Pods-FirebaseAuthTests/Pods-FirebaseAuthTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FirebaseAuthTests : NSObject
-@end
-@implementation PodsDummy_Pods_FirebaseAuthTests
-@end
--- FirebaseAuth/Pods/Target Support Files/Pods-FirebaseAuthUITests/Pods-FirebaseAuthUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FirebaseAuthUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FirebaseAuthUITests
-@end
